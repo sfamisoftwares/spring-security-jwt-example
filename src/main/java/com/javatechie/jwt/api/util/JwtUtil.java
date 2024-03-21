5e0c50c4a38f9ae7516d97d7d4bbb35db2ec6580
javatechie <basanta.kumar.hota.4@gmail.com>
2024-03-21 21:13:41 +0530

    format code

--- src/main/java/com/javatechie/jwt/api/util/JwtUtil.java
+++ src/main/java/com/javatechie/jwt/api/util/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
-    private String secret = "javatechie";
+    private String secret = "javatechie432749327892374";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
